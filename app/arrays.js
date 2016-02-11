b4cf19ad8dc1de9c1ccde851a8f98e6447292b14
KatoCode23 <Kalen23Imani@gmail.com>
2016-02-10 23:19:00 -0500

    made 4 array tests pass

--- app/arrays.js
+++ app/arrays.js
@@ -4,14 +4,27 @@ exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
 
-  },
-
+},
+  
   sum : function(arr) {
+    var result=0
+
+
+    return result;
 
   },
 
   remove : function(arr, item) {
+    var result=[];
+    for (i=0; i < arr.length; i++){
+        
+      if(arr[i]!== item){
+        result.push(arr[i])
+      }
 
+    }
+
+    return result;
   },
 
   removeWithoutCopy : function(arr, item) {
@@ -19,6 +32,9 @@ exports.arraysAnswers = {
   },
 
   append : function(arr, item) {
+    arr.push(item)
+
+    return arr;
 
   },
 
@@ -27,6 +43,9 @@ exports.arraysAnswers = {
   },
 
   prepend : function(arr, item) {
+    arr.unshift(item)
+
+    return arr;
 
   },
 
@@ -39,11 +58,21 @@ exports.arraysAnswers = {
   },
 
   insert : function(arr, item, index) {
+    arr.splice(index, 0, item);
+
+    return arr;
 
   },
 
   count : function(arr, item) {
+    var result=0;
+    for (i=0; i<arr.length; i++){
+      if(arr[i]===item){
+        result = result+1
+      }
 
+    }
+    return result;
   },
 
   duplicates : function(arr) {
